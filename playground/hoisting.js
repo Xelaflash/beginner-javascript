c946fc23b8ad30196af7b73c3c8ca1b46c085bf9
Alexandre Gros <xelaflash@gmail.com>
2020-01-08 12:27:45 -0400

    on avance ds les vvids

--- playground/hoisting.js
+++ playground/hoisting.js
@@ -1,5 +1,10 @@
-/* esline-disable */
+/* eslint-disable */
+
+// !! Hoisting permits to access function/variable before they are created
+
+// !! variable Hoisting ==>
 let age;
+//  JS will create above line as variable declaration
 console.log(age);
 age = 10;
 
@@ -12,4 +17,5 @@ function sayHi() {
   console.log(add(10, 2));
 }
 
+// will not be hoist. only classing function declaration.
 const add = (a, b) => a + b;
--- playground/scope.js
+++ playground/scope.js
@@ -1,50 +1,52 @@
-// const age = 100;
+// ?? var is global variable function scoped ==> Available everywhere
+// !! let & const are block scoped ==> Available in the scope (block or function)
 
-// function go() {
-//   const myAge = 200;
-//   const hair = 'blonde';
-//   console.log(age);
-//   console.log(myAge);
-//   console.log(hair);
-// }
+const age = 100;
 
-// go();
-/* eslint-disable*/
+function go() {
+  const myAge = 200;
+  const hair = 'blonde';
+  console.log(age);
+  console.log(myAge);
+  console.log(hair);
+}
 
-// function isCool(name) {
-//   if (name === 'wes') {
-//     var cool = true;
-//   }
-//   console.log(cool);
-//   return cool;
-// }
+go();
 
-// for(let i = 0; i < 10; i++) {
-//   console.log(i);
-// }
+function isCool(name) {
+  if (name === 'wes') {
+    var cool = true;
+  }
+  console.log(cool);
+  return cool;
+}
 
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
 
+// scope look up ==> lexically. It cares where it's defined NOT where it's run
 const dog = 'snickers';
 
 function logDog(dog) {
   console.log(dog);
 }
 
-function go() {
+function go2() {
   const dog = 'sunny';
   logDog('sunny');
 }
 
-go();
+go2();
 
 function yell() {
-  console.log(name.toUpperCase())
+  console.log(name.toUpperCase());
 }
 
 function sayHi(name) {
-
   yell();
 }
 
-
 yell();
+
+// !! Function are also function scoped.
