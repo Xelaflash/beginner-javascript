fc1d0efce83838a921a2f62f38101532239647e9
Alexandre Gros <xelaflash@gmail.com>
2020-03-02 11:51:54 -0400

    exo done

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -11,19 +11,14 @@ function handleCardButtonClick(event) {
   const name = card.querySelector('h2').textContent;
   // populate the modal with the new info
   modalInner.innerHTML = `
-    <img width="600" height="600" src="${imgSrc.replace(
-      '200',
-      '600'
-    )}" alt="${name}"/>
+    <img width="600" height="600" src="${imgSrc.replace('200', '600')}" alt="${name}"/>
     <p>${desc}</p>
   `;
   // show the modal
   modalOuter.classList.add('open');
 }
 
-cardButtons.forEach(button =>
-  button.addEventListener('click', handleCardButtonClick)
-);
+cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
 
 function closeModal() {
   modalOuter.classList.remove('open');
--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,18 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = document.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(document.querySelectorAll('[role="tabpanel"]'));
+
+function HandletabClick(event) {
+  // hidden all tab panels
+  tabPanels.forEach(panel => (panel.hidden = true));
+  // mark all tab as unselected
+  tabButtons.forEach(tab => tab.setAttribute('aria-selected', false));
+  //  mark the clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+  // find associated tab panel and show it
+  const { id } = event.currentTarget;
+  const tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labelledby') === id);
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', HandletabClick));
