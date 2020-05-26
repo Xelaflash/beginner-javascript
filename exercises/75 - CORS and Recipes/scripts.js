606609caf3a2eb313fd658d7b50d966b3d7c56bf
Alexandre Gros <xelaflash@gmail.com>
2020-05-26 09:25:24 -0400

    done

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,19 +1,26 @@
 const baseEndpoint = 'http://www.recipepuppy.com/api';
-const proxy = `https://cors-anywhere.herokuapp.com/`;
+const proxy = `https://cors-anywhere.herokuapp.com`;
 const form = document.querySelector('form.search');
 const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
-  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
-  const data = await res.json();
+  const response = await fetch(`${proxy}/${baseEndpoint}?q=${query}`);
+  const data = await response.json();
   return data;
 }
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  const el = event.currentTarget;
-  console.log(form.query.value);
-  fetchAndDisplay(form.query.value);
+function displayRecipes(recipes) {
+  const html = recipes.map(
+    recipe => `
+  <div class="recipe">
+  <h2>${recipe.title}</h2>
+  <p>${recipe.ingredients}</p>
+  ${recipe.thumbnail && `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+  <a href="${recipe.href}">View recipe → </a>
+  </div>
+  `
+  );
+  recipesGrid.innerHTML = html.join('');
 }
 
 async function fetchAndDisplay(query) {
@@ -26,20 +33,11 @@ async function fetchAndDisplay(query) {
   displayRecipes(recipes.results);
 }
 
-function displayRecipes(recipes) {
-  console.log('Creating HTML');
-  const html = recipes.map(
-    recipe => `<div class="recipe">
-      <h2>${recipe.title}</h2>
-      <p>${recipe.ingredients}</p>
-      ${recipe.thumbnail &&
-        `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
-      <a href="${recipe.href}">View Recipe →</a>
-    </div>`
-  );
-  recipesGrid.innerHTML = html.join('');
+async function handleSubmit(event) {
+  event.preventDefault();
+  fetchAndDisplay(form.query.value);
 }
 
 form.addEventListener('submit', handleSubmit);
-// on page load run it with pizza
+// run on page load
 fetchAndDisplay('pizza');
